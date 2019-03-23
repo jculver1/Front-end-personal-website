102802c1177a7cdafefcff19f7199be0af888aff
jculver1 <jculver1900@gmail.com>
2019-03-23 17:43:48 -0600

    acded pageScroll to project, now navigates to different section of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  //may need import for pagescroll
 })
+
+
 export class AppComponent {
   title = 'frontend';
 }
